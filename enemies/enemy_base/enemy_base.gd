0791113ef2f82ef1ed2da2815d910624b4c429e5
leobewater <mmbee888@gmail.com>
2023-12-25 17:54:42 -0500

    remove enemy after y position exceeds offset

--- enemies/enemy_base/enemy_base.gd
+++ enemies/enemy_base/enemy_base.gd
@@ -1,20 +1,31 @@
 extends CharacterBody2D
 
-
 class_name EnemyBase
-
 
 enum FACING { LEFT = -1, RIGHT = 1}
 
+# kill element based on this offset
+const OFF_SCREEN_KILL_ME: float = 1000.0
 
 @export var default_facing: FACING = FACING.LEFT
+@export var points: int = 1
+
+var _speed: float = 30.0
+var _gravity: float = 800.0
+var _facing: FACING = default_facing
+var _player_ref: Player
+var _dying: bool = false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_player_ref = get_tree().get_nodes_in_group(GameManager.GROUP_PLAYER)[0]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	fallen_off()
+
+
+func fallen_off() -> void:
+	# remove element after y position if greater than offset
+	if global_position.y > OFF_SCREEN_KILL_ME:
+		queue_free()
