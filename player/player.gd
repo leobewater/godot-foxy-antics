59829232e8a58b8cf632b85dc1bbf3a6296ba6e5
leobewater <mmbee888@gmail.com>
2023-12-25 14:23:31 -0500

    add player movement logic

--- player/player.gd
+++ player/player.gd
@@ -24,8 +24,20 @@ func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
 
+	get_input()
 	move_and_slide()
 
 
 func get_input() -> void:
 	velocity.x = 0
+	
+	if Input.is_action_pressed('left'):
+		velocity.x = -RUN_SPEED
+	elif Input.is_action_pressed('right'):
+		velocity.x = RUN_SPEED
+	
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# limit the player y velocity 
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
