4a964029c3ae32a29353a697baa291943401f6e6
leobewater <mmbee888@gmail.com>
2023-12-25 14:08:02 -0500

    make camera follows player's position

---
+++ level_base/level_base.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var player_cam = $PlayerCam
+@onready var player = $Player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(delta):
+	# make camera follows player
+	player_cam.position = player.position
