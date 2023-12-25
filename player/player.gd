9f2dfdcbbfa1d1bcfab4a13d6db794aa872a5171
leobewater <mmbee888@gmail.com>
2023-12-25 15:42:08 -0500

    add player jump and landing sounds

--- player/player.gd
+++ player/player.gd
@@ -7,6 +7,7 @@ class_name Player
 @onready var debug_label = $DebugLabel
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var sound_player = $SoundPlayer
 
 
 
@@ -57,6 +58,7 @@ func get_input() -> void:
 	
 	if Input.is_action_just_pressed('jump') and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		SoundManager.play_clip(sound_player, SoundManager.SOUND_JUMP)
 		
 	# limit the player y velocity 
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
@@ -83,6 +85,10 @@ func set_state(new_state: PLAYER_STATE) -> void:
 	if new_state == _state:
 		return
 		
+	if _state == PLAYER_STATE.FALL:
+		if new_state == PLAYER_STATE.IDLE or new_state == PLAYER_STATE.RUN:
+			SoundManager.play_clip(sound_player, SoundManager.SOUND_LAND)
+	
 	_state = new_state
 	
 	# play animation based on player_state
