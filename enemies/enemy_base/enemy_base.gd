00d923f5884801f977d049bc9f4f914f83705ae3
leobewater <mmbee888@gmail.com>
2023-12-25 17:49:46 -0500

    add enemybase script

---
+++ enemies/enemy_base/enemy_base.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+class_name EnemyBase
+
+
+enum FACING { LEFT = -1, RIGHT = 1}
+
+
+@export var default_facing: FACING = FACING.LEFT
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
