9d928a8d355d688ee926a618ff3e9c888e36f0a2
leobewater <mmbee888@gmail.com>
2023-12-26 10:33:53 -0500

    set up script for eagle

---
+++ enemies/eagle/eagle.gd
@@ -0,0 +1,9 @@
+extends EnemyBase
+
+
+func _physics_process(delta):
+	super._physics_process(delta)
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	pass # Replace with function body.
