3210b92205e0ce561c2d9727e24e96e976c9cc6a
leobewater <mmbee888@gmail.com>
2023-12-26 11:35:08 -0500

    play eagle fly animation when enter screen

--- enemies/eagle/eagle.gd
+++ enemies/eagle/eagle.gd
@@ -1,9 +1,13 @@
 extends EnemyBase
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var player_detector = $PlayerDetector
+@onready var direction_timer = $DirectionTimer
+
 
 func _physics_process(delta):
 	super._physics_process(delta)
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered():
-	pass # Replace with function body.
+	animated_sprite_2d.play('fly')
