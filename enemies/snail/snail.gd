ed06a082de868069098f98610b88eb27afda1329
leobewater <mmbee888@gmail.com>
2023-12-25 22:19:10 -0500

    check snail hits wall or gap, flip direction

--- enemies/snail/snail.gd
+++ enemies/snail/snail.gd
@@ -1,4 +1,36 @@
 extends EnemyBase
 
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var floor_detection = $FloorDetection
+
+
 func _ready():
 	pass
+
+
+func _physics_process(delta):
+	# call parent class _physics_process()
+	super._physics_process(delta)
+	
+	if is_on_floor() == false:
+		velocity.y += _gravity * delta
+	else:
+		velocity.x = speed * _facing
+		
+	move_and_slide()
+	
+	# check if snail hits wall or ray cast not hitting empty ground
+	if is_on_wall() == true or floor_detection.is_colliding() == false:
+		flip_me()
+		
+
+func flip_me() -> void: 
+	# flip the snail sprite and ray cast
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h 
+	floor_detection.position.x = floor_detection.position.x * -1
+
+	if _facing == FACING.LEFT:
+		_facing = FACING.RIGHT
+	else:
+		_facing = FACING.LEFT
