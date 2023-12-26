69fd8c05505daaa30d8106057c63070ea7368489
leobewater <mmbee888@gmail.com>
2023-12-26 17:17:26 -0500

    add boss take damage and call a function when boss hit animation is finished

--- boss/boss.gd
+++ boss/boss.gd
@@ -15,6 +15,7 @@ const HIT_CONDITION: String = "parameters/conditions/on_hit"
 
 var _invincible: bool = false
 
+
 func _ready():
 	pass # Replace with function body.
 
@@ -23,7 +24,43 @@ func _process(delta):
 	pass
 
 
+func tween_hit() -> void:
+	# when boss is hit, tween the position to 0
+	var tween = get_tree().create_tween()
+	tween.tween_property(visual, "position", Vector2.ZERO, 1.0)
+
+
+func reduce_lives() -> void:
+	lives -= 1
+	print("Boss reduce_lives:", lives)
+	if lives <= 0:
+		SignalManager.on_boss_killed.emit(points)
+		print("Boss dead")
+		set_process(false)
+		queue_free()
+
+
+func set_invincible(v: bool) -> void:
+	print("Boss set_invincible:", v)
+	_invincible = v
+	animation_tree[HIT_CONDITION] = v
+
+
+func take_damage() -> void:
+	if _invincible == true:
+		return
+		
+	set_invincible(true)
+	tween_hit()
+	reduce_lives()
+
+
 # when trigger is collided
 func _on_trigger_area_entered(area):
 	if animation_tree[TRIGGER_CONDITION] == false:
 		animation_tree[TRIGGER_CONDITION] = true
+
+
+# when boss hitbox is collided
+func _on_hit_box_area_entered(area):
+	take_damage()
