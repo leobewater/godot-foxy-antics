8b9a9c5bfba54b32ae01330fa7bd181a8a0c4c8e
leobewater <mmbee888@gmail.com>
2023-12-26 11:41:37 -0500

    set eagle flying direction based on player's x position

--- enemies/eagle/eagle.gd
+++ enemies/eagle/eagle.gd
@@ -4,10 +4,38 @@ extends EnemyBase
 @onready var player_detector = $PlayerDetector
 @onready var direction_timer = $DirectionTimer
 
+const FLY_SPEED: Vector2 = Vector2(35, 15)
+
+var _fly_direction: Vector2 = Vector2.ZERO
+
 
 func _physics_process(delta):
 	super._physics_process(delta)
+	velocity = _fly_direction
+	move_and_slide()
 
 
+# set the flying direction based on player's x position
+func set_and_flip() -> void:
+	var x_dir = sign(_player_ref.global_position.x - global_position.x)
+	if x_dir > 0:
+		animated_sprite_2d.flip_h = true
+	else:
+		animated_sprite_2d.flip_h = false
+	_fly_direction = Vector2(x_dir, 1) * FLY_SPEED
+	
+
+func fly_to_player() -> void:
+	set_and_flip()
+	direction_timer.start()
+
+
+# when eagle enters screen, play the fly animation
 func _on_visible_on_screen_notifier_2d_screen_entered():
 	animated_sprite_2d.play('fly')
+	fly_to_player()
+
+
+# fly for 3s then detect the player position and fly to that direction again
+func _on_direction_timer_timeout():
+	fly_to_player()
