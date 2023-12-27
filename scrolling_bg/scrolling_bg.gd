afc3bc3022c61842da9f0c31eb86b4d8bf17bdae
leobewater <mmbee888@gmail.com>
2023-12-27 09:22:01 -0500

    preload background in scrollingbg

---
+++ scrolling_bg/scrolling_bg.gd
@@ -0,0 +1,47 @@
+extends ParallaxBackground
+
+
+var BG_FILES = {
+	1: [
+		preload("res://assets/backgrounds/game_background_1/layers/sky.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/clouds_1.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/clouds_2.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/clouds_3.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/clouds_4.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/rocks_1.png"),
+		preload("res://assets/backgrounds/game_background_1/layers/rocks_2.png")
+	],
+	2: [
+		preload("res://assets/backgrounds/game_background_2/layers/sky.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/birds.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/clouds_1.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/clouds_2.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/clouds_3.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/pines.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/rocks_1.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/rocks_2.png"),
+		preload("res://assets/backgrounds/game_background_2/layers/rocks_3.png")
+	],
+	3: [
+		preload("res://assets/backgrounds/game_background_3/layers/sky.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/clouds_1.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/clouds_2.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/ground_1.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/ground_2.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/ground_3.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/plant.png"),
+		preload("res://assets/backgrounds/game_background_3/layers/rocks.png")
+	],
+	4: [
+		preload("res://assets/backgrounds/game_background_4/layers/sky.png"),
+		preload("res://assets/backgrounds/game_background_4/layers/clouds_1.png"),
+		preload("res://assets/backgrounds/game_background_4/layers/clouds_2.png"),
+		preload("res://assets/backgrounds/game_background_4/layers/ground.png"),
+		preload("res://assets/backgrounds/game_background_4/layers/rocks.png")
+	]
+}
+
+
+func _ready():
+	pass # Replace with function body.
+
