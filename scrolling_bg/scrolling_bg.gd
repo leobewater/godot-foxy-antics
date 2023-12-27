06faf8525c8130e7831eb8e7265ccd369b01c3c9
leobewater <mmbee888@gmail.com>
2023-12-27 09:28:45 -0500

    auto set up parallax backgrounds per level

--- scrolling_bg/scrolling_bg.gd
+++ scrolling_bg/scrolling_bg.gd
@@ -42,6 +42,51 @@ var BG_FILES = {
 }
 
 
-func _ready():
-	pass # Replace with function body.
+@export_range(1, 4) var level_number: int
+@export var mirror_x: float = 1440.0
+@export var sprite_offset: Vector2 = Vector2(0, -540)
+@export var sprite_scale: Vector2 = Vector2(0.75, 075)
 
+
+func _ready():
+	add_backgrounds()
+
+
+func get_increment() -> float:
+	return 1.0 / BG_FILES[level_number].size()
+	
+
+func get_sprite(t: Texture2D) -> Sprite2D:
+	var sprite = Sprite2D.new()
+	sprite.texture = t
+	sprite.scale = sprite_scale
+	sprite.offset = sprite_offset
+	return sprite
+
+
+func add_layer(t: Texture2D, time_offset: float) -> void:
+	var sprite = get_sprite(t)
+	
+	# make parallax layer
+	var par_la = ParallaxLayer.new()
+	par_la.motion_mirroring = Vector2(mirror_x, 0)
+	par_la.motion_scale = Vector2(time_offset, 1)
+	# add sprite to parallax layer
+	par_la.add_child(sprite)
+	
+	add_child(par_la)
+	
+	
+func add_backgrounds() -> void:
+	var inc = get_increment()
+	var time_offset = inc
+	var files_list = BG_FILES[level_number]
+	
+	for index in range(files_list.size()):
+		var bg_file = files_list[index]
+		if index == 0:
+			add_layer(bg_file, 1)
+		else:
+			add_layer(bg_file, time_offset)
+			time_offset += inc
+	
