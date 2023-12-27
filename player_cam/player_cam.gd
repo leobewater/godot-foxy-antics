de437a9aa2f73502faed94b6b7ed1264cc5e22a2
leobewater <mmbee888@gmail.com>
2023-12-27 09:42:06 -0500

    shake camera when player gets hurt

--- player_cam/player_cam.gd
+++ player_cam/player_cam.gd
@@ -1,9 +1,41 @@
 extends Camera2D
 
 
+@onready var shake_timer = $ShakeTimer
+
+
+@export var shake_amount: float = 5.0
+
+
 func _ready():
-	pass # Replace with function body.
+	set_process(false)
+	SignalManager.on_player_hit.connect(on_player_hit)
 
 
 func _process(delta):
-	pass
+	offset = get_random_offset()
+
+
+func get_random_offset() -> Vector2:
+	return Vector2(
+		randf_range(-shake_amount, shake_amount),
+		randf_range(-shake_amount, shake_amount)
+	)
+
+
+# shake camera when player gets hit
+func shake() -> void:
+	set_process(true)
+	shake_timer.start()
+	set_process(false)
+
+
+func on_player_hit(_lives:int) -> void:
+	shake()
+
+
+func _on_shake_timer_timeout():
+	set_process(false)
+	offset = Vector2.ZERO
+
+
