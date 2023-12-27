f59932b0d795b29cb19baf36bd2505e2b6a0674f
leobewater <mmbee888@gmail.com>
2023-12-27 11:09:44 -0500

    test loading main and next level scenes

--- level_base/level_base.gd
+++ level_base/level_base.gd
@@ -7,14 +7,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Engine.time_scale = 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _physics_process(delta):
+func _process(_delta):
 	# make camera follows player
 	player_cam.position = player.position
+	
+	# test loading scenes
+	'''
+	if Input.is_action_just_pressed("left"):
+		GameManager.load_main_scene()
+	if Input.is_action_just_pressed("right"):
+		GameManager.load_next_level_scene()
+	'''
