46ffb8c8041b22e16fe6be6bf79007b952837b86
leobewater <mmbee888@gmail.com>
2023-12-27 11:45:25 -0500

    listen signal for scoring changes

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -10,10 +10,30 @@ var _high_score: int = 0
 
 func _ready():
 	load_highscore()
+	SignalManager.on_boss_killed.connect(on_boss_killed)
+	SignalManager.on_pickup_hit.connect(on_pickup_hit)
+	SignalManager.on_enemy_hit.connect(on_enemy_hit)
+	
+	SignalManager.on_level_complete.connect(on_level_complete)
+	SignalManager.on_game_over.connect(on_game_over)
 
 
-func _process(delta):
-	pass
+func update_score(points: int) -> void:
+	_score += points
+	if _high_score < _score:
+		_high_score = _score
+
+
+func get_score() -> int:
+	return _score
+
+
+func get_high_score() -> int:
+	return _high_score
+
+
+func reset_score() -> void:
+	_score = 0
 
 
 func save_highscore() -> void:
@@ -39,3 +59,17 @@ func load_highscore() -> void:
 		print("loaded _high_score:", _high_score)
 
 
+func on_level_complete() -> void:
+	save_highscore()
+
+func on_game_over() -> void:
+	save_highscore()
+
+func on_boss_killed(p: int) -> void:
+	update_score(p)
+
+func on_pickup_hit(p: int) -> void:
+	update_score(p)
+
+func on_enemy_hit(p: int, _v: Vector2) -> void:
+	update_score(p)
